e89aca3b99fe01d475a9a16140a97941ce42b4ef
robertsben <roberts.ben@me.com>
2019-06-07 20:06:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## homepage
+
+A homepage website built using nothing but standard library tools.
---
+++ home_python/src/app.py
@@ -0,0 +1,14 @@
+"""
+Main app module
+"""
+
+
+def main():
+    """
+    Start function
+    """
+    print('hi!')
+
+
+if __name__ == '__main__':
+    main()
